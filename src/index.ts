cf4ca383812ddbc717742a9e55cb654dd5ae8e27
poechiang <poechiang@live.cn>
2023-01-14 09:18:07 +0800

    init...

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,53 @@
-import { format } from 'date-fns';
-import { noop } from 'lodash';
-import { buildOutputPreffix, checkEnv, checkPlatform } from './utils';
+import { format } from "date-fns";
+import { Logger } from "./interface";
+import { buildOutputPreffix, checkEnv, checkPlatform, noop } from "./utils";
 
 const defOptions: Logger.IOptions = {
-    date: true,
-    env: 'all',
+  date: true,
+  env: Logger.Evn.ALL,
 };
 
 export const loggerWithTags = (
-    tags: string | string[],
-    options?: Logger.IOptions,
+  tags: string | string[],
+  options?: Logger.IOptions
 ) => {
-    if (!Array.isArray(tags)) {
-        tags = [tags];
+  if (!Array.isArray(tags)) {
+    tags = [tags];
+  }
+
+  const { env, ...mergedOptions } = { ...defOptions, ...(options || {}) };
+
+  const debug = (...rest: any[]) => {
+    const prefix = buildOutputPreffix(tags as string[], mergedOptions);
+    checkEnv(env || Logger.Evn.ALL) && console.debug(prefix, ...rest);
+  };
+  const info = (...rest: any[]) => {
+    const prefix = buildOutputPreffix(tags as string[], mergedOptions);
+    checkEnv(env || Logger.Evn.ALL) && console.info(prefix, ...rest);
+  };
+  const log = (...rest: any[]) => {
+    const prefix = buildOutputPreffix(tags as string[], mergedOptions);
+    checkEnv(env || Logger.Evn.ALL) && console.log(...prefix, ...rest);
+    checkPlatform().then((write) =>
+      write?.(
+        `logs/${format(Date.now(), "yyyy-MM-dd")}.log`,
+        [...prefix, ...rest.map(JSON.stringify as any), "\n"].join(" "),
+        { flag: "a" },
+        noop
+      )
+    );
+  };
+  const warn = (...rest: any[]) => {
+    const prefix = buildOutputPreffix(tags as string[], mergedOptions);
+    checkEnv(env || Logger.Evn.ALL) && console.warn(prefix, ...rest);
+  };
+
+  const error = (msg: string) => {
+    const prefix = buildOutputPreffix(tags as string[], mergedOptions);
+    if (checkEnv(env || Logger.Evn.ALL)) {
+      console.warn(prefix);
+      throw new Error(msg);
     }
-
-    const { env, ...mergedOptions } = { ...defOptions, ...(options || {}) };
-
-    const debug = (...rest: any[]) => {
-        const prefix = buildOutputPreffix(tags as string[], mergedOptions);
-        checkEnv(env || 'all') && console.debug(prefix, ...rest);
-    };
-    const info = (...rest: any[]) => {
-        const prefix = buildOutputPreffix(tags as string[], mergedOptions);
-        checkEnv(env || 'all') && console.info(prefix, ...rest);
-    };
-    const log = (...rest: any[]) => {
-        const prefix = buildOutputPreffix(tags as string[], mergedOptions);
-        checkEnv(env || 'all') && console.log(...prefix, ...rest);
-        checkPlatform().then((write) =>
-            write?.(
-                `logs/${format(Date.now(), 'yyyy-MM-dd')}.log`,
-                [...prefix, ...rest.map(JSON.stringify as any), '\n'].join(' '),
-                { flag: 'a' },
-                noop,
-            ),
-        );
-    };
-    const warn = (...rest: any[]) => {
-        const prefix = buildOutputPreffix(tags as string[], mergedOptions);
-        checkEnv(env || 'all') && console.warn(prefix, ...rest);
-    };
-
-    const error = (msg: string) => {
-        const prefix = buildOutputPreffix(tags as string[], mergedOptions);
-        if (checkEnv(env || 'all')) {
-            console.warn(prefix);
-            throw new Error(msg);
-        }
-    };
-    return { debug, info, log, warn, error };
+  };
+  return { debug, info, log, warn, error };
 };
