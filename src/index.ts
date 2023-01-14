f4523e369f1c49548ec37ae42550fe0d436d8cf8
poechiang <poechiang@live.cn>
2023-01-14 11:03:27 +0800

    init...

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,13 @@
-import { format } from "date-fns";
-<<<<<<< HEAD
-import { Logger } from "./interface";
-=======
->>>>>>> 30c7cd43c5261d473f011ebdc28cde4e2779712e
-import { buildOutputPreffix, checkEnv, checkPlatform, noop } from "./utils";
+import { format } from 'date-fns';
+import { Logger } from './interface';
+import { buildOutputPreffix, checkEnv, checkPlatform, noop } from './utils';
 
 const defOptions: Logger.IOptions = {
   date: true,
-<<<<<<< HEAD
   env: Logger.Evn.ALL,
-=======
-  env: "all",
->>>>>>> 30c7cd43c5261d473f011ebdc28cde4e2779712e
 };
 
-export const loggerWithTags = (
-  tags: string | string[],
-  options?: Logger.IOptions
-) => {
+export const loggerWithTags = (tags: string | string[], options?: Logger.IOptions) => {
   if (!Array.isArray(tags)) {
     tags = [tags];
   }
@@ -26,7 +16,6 @@ export const loggerWithTags = (
 
   const debug = (...rest: any[]) => {
     const prefix = buildOutputPreffix(tags as string[], mergedOptions);
-<<<<<<< HEAD
     checkEnv(env || Logger.Evn.ALL) && console.debug(prefix, ...rest);
   };
   const info = (...rest: any[]) => {
@@ -36,42 +25,23 @@ export const loggerWithTags = (
   const log = (...rest: any[]) => {
     const prefix = buildOutputPreffix(tags as string[], mergedOptions);
     checkEnv(env || Logger.Evn.ALL) && console.log(...prefix, ...rest);
-=======
-    checkEnv(env || "all") && console.debug(prefix, ...rest);
-  };
-  const info = (...rest: any[]) => {
-    const prefix = buildOutputPreffix(tags as string[], mergedOptions);
-    checkEnv(env || "all") && console.info(prefix, ...rest);
-  };
-  const log = (...rest: any[]) => {
-    const prefix = buildOutputPreffix(tags as string[], mergedOptions);
-    checkEnv(env || "all") && console.log(...prefix, ...rest);
->>>>>>> 30c7cd43c5261d473f011ebdc28cde4e2779712e
     checkPlatform().then((write) =>
       write?.(
-        `logs/${format(Date.now(), "yyyy-MM-dd")}.log`,
-        [...prefix, ...rest.map(JSON.stringify as any), "\n"].join(" "),
-        { flag: "a" },
-        noop
-      )
+        `logs/${format(Date.now(), 'yyyy-MM-dd')}.log`,
+        [...prefix, ...rest.map(JSON.stringify as any), '\n'].join(' '),
+        { flag: 'a' },
+        noop,
+      ),
     );
   };
   const warn = (...rest: any[]) => {
     const prefix = buildOutputPreffix(tags as string[], mergedOptions);
-<<<<<<< HEAD
     checkEnv(env || Logger.Evn.ALL) && console.warn(prefix, ...rest);
-=======
-    checkEnv(env || "all") && console.warn(prefix, ...rest);
->>>>>>> 30c7cd43c5261d473f011ebdc28cde4e2779712e
   };
 
   const error = (msg: string) => {
     const prefix = buildOutputPreffix(tags as string[], mergedOptions);
-<<<<<<< HEAD
     if (checkEnv(env || Logger.Evn.ALL)) {
-=======
-    if (checkEnv(env || "all")) {
->>>>>>> 30c7cd43c5261d473f011ebdc28cde4e2779712e
       console.warn(prefix);
       throw new Error(msg);
     }
--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,12 @@
-import { format } from "date-fns";
-<<<<<<< HEAD
-import type { Logger } from "./interface";
-=======
->>>>>>> 30c7cd43c5261d473f011ebdc28cde4e2779712e
+import { format } from 'date-fns';
+import type { Logger } from './interface';
 
 const toString = (v: any): string => Object.prototype.toString.call(v);
 
 export const isUndefined = (v: any) => v === undefined;
-export const isString = (v: any) =>
-  typeof v === "string" || toString(v) === "[object String]";
+export const isString = (v: any) => typeof v === 'string' || toString(v) === '[object String]';
 
-export const buildOutputPreffix = (
-  tags: string[],
-  options: Logger.IOptions
-) => {
+export const buildOutputPreffix = (tags: string[], options: Logger.IOptions) => {
   const prefix = [];
   const { date } = options;
   if (date) {
@@ -25,16 +18,15 @@ export const buildOutputPreffix = (
     prefix.unshift(now);
   }
   if (tags.length) {
-    prefix.unshift(tags.map((t) => `[${t.toUpperCase()}]`).join(" "));
+    prefix.unshift(tags.map((t) => `[${t.toUpperCase()}]`).join(' '));
   }
   return prefix;
 };
 
-export const checkEnv = (env: Logger.Evn) =>
-  (env === "all" || process.env.NODE_ENV === env) && console !== undefined;
+export const checkEnv = (env: Logger.Evn) => (env === 'all' || process.env.NODE_ENV === env) && console !== undefined;
 
 export const checkPlatform = () =>
-  import("fs").then((fs) => {
+  import('fs').then((fs) => {
     // fs.opendir('logs')
     return fs?.writeFile;
   });
