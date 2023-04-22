6553e623741e75a6482a6f577625c21c69e080b4
poechiang <poechiang@live.cn>
2023-04-23 06:11:47 +0800

    chore(release): 3.0.1-alpha.3

--- src/utils/checkPlateform.ts
+++ src/utils/checkPlateform.ts
@@ -4,8 +4,11 @@ import { IFileHelper } from '../interface';
  * 检查当前运行环境,node环境下返回node的fs模块,浏览器环境返回null
  * @returns fs
  */
-export const checkPlatform = (): Promise<IFileHelper | null> =>
-  Promise.all([import('fs'), import('path')])
+export const checkPlatform = (): Promise<IFileHelper | null> => {
+  if (typeof window !== 'undefined') {
+    return Promise.resolve(null);
+  }
+  return Promise.all([import('fs'), import('path')])
     .then(([fs, path]) => {
       // 如果fs为空则判定当前为浏览器环境
       if (!fs) return null;
@@ -22,3 +25,4 @@ export const checkPlatform = (): Promise<IFileHelper | null> =>
       };
     })
     .catch(() => null);
+};
