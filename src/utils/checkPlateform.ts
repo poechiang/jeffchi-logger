563be26e2da5a909264424c09e2791ab62e1100b
poechiang <poechiang@live.cn>
2023-04-23 06:22:13 +0800

    chore(release): 3.0.1-alpha.5

--- src/utils/checkPlateform.ts
+++ src/utils/checkPlateform.ts
@@ -6,10 +6,10 @@ import { IFileHelper } from '../interface';
  */
 export const checkPlatform = (): Promise<IFileHelper | null> => {
   if (typeof window !== 'undefined') {
-    console.log(123123, window);
+    console.log(123123);
     return Promise.resolve(null);
   }
-  console.log(456456, window);
+  console.log(456456);
   return Promise.all([import('fs'), import('path')])
     .then(([fs, path]) => {
       // 如果fs为空则判定当前为浏览器环境
